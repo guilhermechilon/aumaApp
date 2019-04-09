f4a99455ff4adc67b4a9ee3afe2f86df2dd9bf1a
Jefferson Nogueira <jeffernog@gmail.com>
2019-04-09 09:59:13 -0300

    Removendo cake

--- backend/BackendNode/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-/*const db = require('./config/db')
-
-app.db = db
-*/
-app.listen(3000, () => {
-    console.log('Backend executando...')
-})
